6eaf7d423b5b9cc188df711b38683b2eb4d150e6
Krzysztof Kiszka <krzysztofkiszka@yahoo.com>
2023-09-05 18:24:42 +0200

    Add supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://lbdqarignbrojneuxgxx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZHFhcmlnbmJyb2puZXV4Z3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NDM1MzAsImV4cCI6MjAwOTQxOTUzMH0.STpDsUQ-Jj35M4T2jlamqbgnwVoj6ny3Vvx7dIIvu8Y";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
