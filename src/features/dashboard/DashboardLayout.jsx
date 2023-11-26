155305b9d135c1c22e1bf6e55bb731333c1ad58e
Krzysztof Kiszka <krzysztofkiszka@yahoo.com>
2023-11-26 17:36:08 +0100

    Add dashboard functions

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import useRecentBookings from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import useRecentStays from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,22 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoading1, bookings } = useRecentBookings();
+  const { isLoading: isLoading2, stays, confirmedStays } = useRecentStays();
+
+  if (isLoading1) return <Spinner />;
+
+  console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <div className="">Statistics</div>
+      <div className="">Today's activity</div>
+      <div className="">Chart stay duration</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
